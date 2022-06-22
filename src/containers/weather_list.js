f9fd3deec0f438f2e6d990e0aa8c1c66e1ad059c
Gee <glynester@hotmail.com>
2022-06-22 23:11:21 +0100

    Extracted temp data and displayed with react-sparklines

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine  } from 'react-sparklines';
 
 class WeatherList extends Component {
 
   renderWeather(cityData){
+    let temps = cityData.list.map(vals=>vals.main.temp)
+    console.log("temps",temps);
     return (
 // rule behind adding a key in a re-act list we add it to the top level element in the list and it just has to be some unique piece of data
       <tr key={cityData.city.id}>
         <td>{cityData.city.name}</td>
+        <td>
+          <Sparklines height={120} width={180} data={temps}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
       </tr>
+      
     )
   }
 
