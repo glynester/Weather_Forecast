add4ebc80019c97f415678a7f609f7b68b212f61
Gee <glynester@hotmail.com>
2022-06-22 15:50:51 +0100

    preventDefault for form submit API request. Redux-promise - middleware for AJAX request. Axios - ajax requests for browser. Data returned from API openweathermap.org

---
+++ src/actions/index.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+// const API_KEY = process.env.REACT_APP_API_KEY; // Doesn't work
+import {REACT_APP_API_KEY} from '../../.env'; // Fudged because env variables not working.
+const API_KEY = REACT_APP_API_KEY;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER='FETCH_WEATHER';
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},uk`;
+  const request = axios.get(url);  // This returns a promise. Behaves like jquery's ajax method.
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
+
+// action creators always have to return an action. And an action is an object which always, always, always has to have a type.
+
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// api.openweathermap.org/data/2.5/forecast?q={city name},{country code}&appid={API key}
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
 class SearchBar extends Component {
   constructor(props){
@@ -6,19 +9,28 @@ class SearchBar extends Component {
     this.state={ term: '' };
     this.onInputChange=this.onInputChange.bind(this); // bind the value of 'this'. "this" is the SearchBar.
     //  then replace onInputChange with this new bound instance of this function.
-
+    this.onFormSubmit=this.onFormSubmit.bind(this);
+  }
   // onInputChange(term){
   //   this.setState({term});
   //   console.log("term=>",this.state.term)
   // }
   onInputChange(event){
     this.setState({ term: event.target.value });
-    console.log("term=>",this.state.term)
+    console.log("term=>",this.state.term);
+  }
+
+  onFormSubmit(event){
+    event.preventDefault();
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ''}); // Will clear out input on form.
+    // console.log("XXX",x);
   }
 
   render(){
     return (
-    <form className="input-group">
+    <form onSubmit={this.onFormSubmit} className="input-group">
+       {/* Std browser behaviour - If you press enter on your keyboard while a form element child (like the input or the button is focused), the browser automatically thinks that you're trying to submit the contents of this form - so makes post request to server. Add event hadler to form to prevent this.*/}
       <input 
         placeholder="Get a 5 day forecast in your favourite cities"
         className='form-control'
@@ -32,7 +44,13 @@ class SearchBar extends Component {
     </form>)
   }
 }
+//  Need to hook up the action creator fetchweather to our search bar container.
+  function mapDispatchToProps(dispatch){
+    // Make sure that that action flows down into the middleware and then the reducers inside of our redux application.
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
 
-export default SearchBar;
+//  mapDispatchToProps always goes in as the second argument.
+export default connect(null, mapDispatchToProps)(SearchBar);
 
 // it definitely needs to be able to talk to Redux which means we definitely are going to want to make it a container because a container has the ability to talk to redux whereas a normal component does not.
