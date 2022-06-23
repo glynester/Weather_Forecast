2a2cc7c190ee4b67798bc445295bde91eb14f0d8
Gee <glynester@hotmail.com>
2022-06-23 09:00:56 +0100

    Graphs prior to refactoring into a separate component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -5,15 +5,27 @@ import { Sparklines, SparklinesLine  } from 'react-sparklines';
 class WeatherList extends Component {
 
   renderWeather(cityData){
-    let temps = cityData.list.map(vals=>vals.main.temp)
+    let temps = cityData.list.map(vals=>vals.main.temp);
+    let pressure = cityData.list.map(vals=>vals.main.pressure);
+    let humidity = cityData.list.map(vals=>vals.main.humidity);
     console.log("temps",temps);
     return (
-// rule behind adding a key in a re-act list we add it to the top level element in the list and it just has to be some unique piece of data
+      // rule behind adding a key in a React list - add it to the top level element in the list and it just has to be some unique piece of data.
       <tr key={cityData.city.id}>
         <td>{cityData.city.name}</td>
         <td>
           <Sparklines height={120} width={180} data={temps}>
             <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
+        <td>
+          <Sparklines height={120} width={180} data={pressure}>
+            <SparklinesLine color="green" />
+          </Sparklines>
+        </td>
+        <td>
+          <Sparklines height={120} width={180} data={humidity}>
+            <SparklinesLine color="purple" />
           </Sparklines>
         </td>
       </tr>
